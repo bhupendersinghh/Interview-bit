cd26452a12cdfe2099a41d4a9ea418de475041e0
Bhupender Singh <bhupender.singhh98@gmail.com>
2019-07-18 00:23:18 +0530

    Created Hotel Reviews.cpp

--- Hotel Reviews.cpp
+++ Hotel Reviews.cpp
@@ -1,12 +1,12 @@
 #define F first
 #define S second
- 
+
 class TrieNode {
     public:
       TrieNode* children[26];
       bool isEnd;
 };
- 
+
 TrieNode* getNode() {
     TrieNode* newNode = new TrieNode;
     newNode->isEnd = false;
@@ -14,7 +14,7 @@ TrieNode* getNode() {
         newNode->children[i] = NULL;
     return newNode;
 }
- 
+
 void insertTrieNode(TrieNode* root, string key) {
     TrieNode* curr = root;
     for(int i = 0; i < key.length(); i++) {
@@ -25,7 +25,7 @@ void insertTrieNode(TrieNode* root, string key) {
     }
     curr->isEnd = true;
 }
- 
+
 bool searchKey(TrieNode* root, string key) {
     TrieNode* curr = root;
     for(int i = 0; i < key.length(); i++) {
@@ -36,7 +36,7 @@ bool searchKey(TrieNode* root, string key) {
     }
     return (curr && curr->isEnd);
 }
- 
+
 vector<string> split(string A) {
     string temp = "";
     vector<string> res;
@@ -54,26 +54,24 @@ vector<string> split(string A) {
     }
     return res;
 }
- 
+
 bool cmp(const pair<int, int>& a, const pair<int, int>& b){
         if(a.F == b.F) return a.S < b.S;
-    return a.F > b.F;
+    return a.F > b.F;    
 }
+
 vector<int> Solution::solve(string A, vector<string> &B) {
     vector<int> sol(B.size());
     vector<string> words = split(A);
-    TrieNode* root = getNode();
-    for(int j = 0; j < words.size(); j++) {
-        insertTrieNode(root, words[j]);
-    }
     vector<pair<int, int> > rating;
     int count;
+    TrieNode* root = getNode();
+    for(int i = 0; i < words.size(); i++) {
+        insertTrieNode(root, words[i]);
+    }
     for(int i = 0; i < B.size(); i++) {
         count = 0;
         vector<string> temp = split(B[i]);
-        // for(int j = 0; j < temp.size(); j++) {
-        //     insertTrieNode(root, temp[j]);
-        // }
         for(int j = 0; j < temp.size(); j++) {
             if(searchKey(root, temp[j])) {
                 count++;
@@ -83,8 +81,8 @@ vector<int> Solution::solve(string A, vector<string> &B) {
     }
     sort(rating.begin(), rating.end(), cmp);
     for(int i = 0; i < B.size(); i++) {
-        sol[i]=rating[i].S;
+        sol[i] = rating[i].S;
     }
-
+    
     return sol;
 }
